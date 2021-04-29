f9ee31065d2a1b642695fa6a70e3702ce0afd8a2
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-04-29 20:57:04 +0000

    Implement logic for create and destroy paths

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by email: (params[:email])
+    # if the user exists AND the password is correct
+    if user && user.authenticate(params[:password])
+      #Save the user id in browser cookie.
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/login'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
   end
 end
