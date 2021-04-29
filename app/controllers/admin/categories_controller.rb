205d03378470263e0dd495d20feaab1c6b4ff55a
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-04-29 16:54:27 +0000

    Create index controller method, and index page

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,8 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD']
+
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+
 end
