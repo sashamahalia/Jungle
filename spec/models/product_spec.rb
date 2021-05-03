7f0f5de26a962920e63be1440f0b1fa07d00b10f
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-05-03 23:05:52 +0000

    Test that Product is valid all required inputs are present

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    subject {
+      described_class.new(name: 'Sasha', price: 8432, quantity: 8, category_id: 1)
+    }
+    it "is valid with name, price, quantity and category present" do
+      expect(subject).to be_valid
+    end
+
+    it "is not valid without a name"
+    it "is not valid without a price"
+    it "is not valid without a quantity"
+    it "is not valid without a category"
+  end
 end
