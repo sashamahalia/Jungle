d8c475cc0c9772c4a4a8f84b3ab287f09b678e3b
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-05-04 03:41:37 +0000

    Implement lowercase for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,12 @@ class User < ActiveRecord::Base
 
   # returns an error if password doesn't match password_confirmmation
   validates_presence_of :password_confirmation, :if => :password_digest_changed?
+  validates :email, uniqueness: true
+  before_validation :normalize_email
+
+  # adapted from https://gist.github.com/flarnie/6257056
+  def normalize_email
+    self.email = self.email.downcase.strip
+  end
 
 end
