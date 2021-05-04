a6242dfb1c33366fb0fe2a27531ae125c460ca09
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-05-04 01:14:28 +0000

    Delete redundant validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  validates_presence_of :password
+  # returns an error if password doesn't match password_confirmmation
   validates_presence_of :password_confirmation, :if => :password_digest_changed?
 
 end
