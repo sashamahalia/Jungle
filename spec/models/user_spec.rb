f54d4342a71cfb479b089ab0f1e4fda0ae1fc15e
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-05-04 01:14:47 +0000

    test non matching passwords

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,5 +9,15 @@ RSpec.describe User, type: :model do
       expect(subject).to be_valid
       expect(subject.password).to eql(subject.password_confirmation)
     end
+    it "is not valid when passwords don't match" do
+      subject.password_confirmation = "okblah"
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages[0]).to eql("Password confirmation doesn't match Password")
+    end
+    it "is not valid when passwords don't match" do
+      subject.password_confirmation = "okblah"
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages[0]).to eql("Password confirmation doesn't match Password")
+    end
   end
 end
